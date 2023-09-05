1165901dc50370024fc6a3a1efbbd72ca94e219b
JackICT16✅ <139789768+JACKICT16@users.noreply.github.com>
2023-09-06 03:15:29 +0700

    อัปเดตโปรเจค

--- lib/views/screens/side_bar_screens/orders_screen.dart
+++ lib/views/screens/side_bar_screens/orders_screen.dart
@@ -4,19 +4,58 @@ import 'package:flutter/material.dart';
 class OrderScreen extends StatelessWidget {
   static const String routeName = '\Ordercreen';
 
+// สร้างตารางของผู้บาย โดยกำหนดหัวข้อ กรอบบน
+  Widget _rowHeader(String text, int flex) {
+    return Expanded(
+      flex: flex,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey.shade700),
+          color: Colors.yellow.shade900,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Orders',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Orders',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
+            ),
           ),
-        ),
+
+          //หัวข้อ row -CITY  STATE แก้เป็น จ่ายยังไง กับ เวลาประมาณการที่จะได้รับ
+          Row(
+            children: [
+              _rowHeader('IMAGE', 1),
+              _rowHeader('FULL NAME', 3),
+              _rowHeader('CITY', 2),
+              _rowHeader('STATE', 2),
+              _rowHeader('ACTION', 1),
+              _rowHeader('VIEW MORE', 1),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/views/screens/side_bar_screens/venders_screen.dart
+++ lib/views/screens/side_bar_screens/venders_screen.dart
@@ -3,19 +3,51 @@ import 'package:flutter/material.dart';
 class VendorScreen extends StatelessWidget {
   static const String routeName = '\VendorScreen';
 
+// สร้างตารางของผู้บาย โดยกำหนดหัวข้อ กรอบบน
+  Widget _rowHeader(String text, int flex) {
+    return Expanded(
+        flex: flex,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey.shade700),
+            color: Colors.yellow.shade900,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(text,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),
+                  ),
+          ),));
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Manage Vendors',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Manage Vendors',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
+            ),
           ),
-        ),
+
+          //หัวข้อ row -CITY  STATE แก้เป็น เวลาเปิดปิด กับ ประเภทอาหาร
+          Row(
+            children: [
+              _rowHeader('LOGO', 1),
+              _rowHeader('BUSSINESS NAME', 3),
+              _rowHeader('CITY', 2),
+              _rowHeader('STATE', 2),
+              _rowHeader('ACTION', 1),
+              _rowHeader('VIEW MORE', 1),
+            ],
+          ),
+        ],
       ),
     );
   }
